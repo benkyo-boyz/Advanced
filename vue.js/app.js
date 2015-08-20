54112a239605a8e383ab5e412a21077ccb4cc043
太田 智彬 <tomoaki_ota@r.recruit.co.jp>
2015-08-20 14:06:51 +0900

    stash

--- vue.js/app.js
+++ vue.js/app.js
@@ -1,4 +1,29 @@
-var data = {
-  users: ["erina_rmp", "ono", "ryo_sakai", "yukinakane", "h.nakazato"],
-  keywords: [ "mvvm", "browser", "framework" ]
-}
+Vue.component('car-component', {
+  template: '#car-component',
+  props: ['catalogCar']
+});
+
+var vm = new Vue({
+  el: '#vue-app',
+
+  data: {
+    sortKey: 'name'
+  },
+
+  created: function() {
+    var url = 'http://www.carsensorlab.net/webapi/V2/catalogSearch/?output=json&brand=' + encodeURIComponent('フェラーリ');
+    console.log(url);
+    this.$http.jsonp(url, function (data, status, request) {
+      this.$set('catalogCar', data.catalogCar);
+    }).error(function (data, status, request) {
+      console.error(status);
+    });
+  },
+
+  methods: {
+    onClick: function (sortKey) {
+      this.sortKey = sortKey;
+    }
+  }
+});
+
