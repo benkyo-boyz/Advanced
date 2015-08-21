e4159c0a0bde80e317256229a0857dab27b17fb1
太田 智彬 <tomoaki_ota@r.recruit.co.jp>
2015-08-21 18:16:38 +0900

    updated accordion

--- vue.js/src/app.js
+++ vue.js/src/app.js
@@ -4,13 +4,32 @@ var carComponent = require('./views/car-component.html');
 
 Vue.component('car-component', {
   template: carComponent,
-  props: ['catalogCar']
+
+  props: ['catalogCar'],
+
+  data() {
+    return {
+      toggle: false
+    }
+  },
+
+  methods: {
+    accordion() {
+      this.toggle = !this.toggle;
+      event.preventDefault();
+    }
+  }
 });
 
 var vm = new Vue({
   el: '#vue-app',
 
-  ready: function() {
+  data: {
+    catalogCar: {},
+    toggle: false
+  },
+
+  ready() {
     var url = 'http://www.carsensorlab.net/webapi/V2/catalogSearch/?output=json&brand=' + encodeURIComponent('フェラーリ');
     this.$http.jsonp(url, function (data, status, request) {
       this.$set('catalogCar', data.catalogCar);
